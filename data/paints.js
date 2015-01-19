b18f314188b01a7371179036b0d53e093b227312
twissell <avb5260@gmail.com>
2015-01-19 14:57:03 +0100

    commit

---
+++ data/paints.js
@@ -0,0 +1,12 @@
+var paints = [
+  {
+    title: 'fun on the moon',
+    src:   'fun.jpg'
+  },
+  {
+    title: 'wide',
+    src:   'painting-wide.jpg'
+  }
+]
+
+module.exports = paints;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var paints = require('../data/paints');
 
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/paints', function(req, res) {
+  res.json(paints);
 });
 
 module.exports = router;
